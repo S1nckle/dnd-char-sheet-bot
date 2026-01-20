f7e75203064ae33c2f1c5e0f943b87d0d072f6e2
S1nckle <LeoSerg2007@gmail.com>
2026-01-20 23:22:41 +0500

    20.01.26

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
+import atexit
+
+import telebot
+
 from playersheetapi import *
 
-sheet = PlayerSheet()
-sheet.fill_header()
-sheet.fill_stats()
+file = open('bot_token.txt', 'r')
+TOKEN = file.readline()
+file.close()
 
-print(sheet)
+bot = telebot.TeleBot(TOKEN)
+sheet_list = SheetsList()
+
+atexit.register(sheet_list.dump)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    user = message.from_user.id
+    text = \
+        '''Привет!
+Этот бот создан для быстрого доступа к нескольким листам персонажа в цифровом формате.
+Сейчас взглянем на твои листы...\n'''
+    sls = sheet_list.user_sheets(user)
+    if sls:
+        text = text + f'У тебя есть {len(sls)} созданных листов!\nИспользуй команду /pick чтобы выбрать лист.'
+    else:
+        text = text + f'Кажется, у тебя еще нет созданных листов!\nПопробуй создать свой первый с помощью /create'
+    bot.send_message(message.from_user.id, text=text)
+
+
+@bot.message_handler(commands=['create'])
+def create_sheet(message):
+    sheet_list.add_user_sheet(message.from_user.id, PlayerSheet())
+    sheet_list.pick(message.from_user.id, sheet_list.user_sheets(message.from_user.id)[0])
+    bot.send_message(message.from_user.id, text='Персонаж создан!')
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
--- playersheetapi/playersheet.py
+++ playersheetapi/playersheet.py
@@ -17,17 +17,65 @@ class PlayerSheet:
         return '\n'.join(str(item) for item in
                          (self.header_container, self.stats_container, self.combat_container, self.attacks_and_spells))
 
-    def fill_header(self):
-        self.header_container.set_char_name(input('Введите имя героя: '))
-        self.header_container.add_class(input('Введите первый класс: '))
-        self.header_container.set_player_name(input('Введите имя игрока: '))
-        self.header_container.set_background(input('Введите предысторию (Одним словом): '))
-        self.header_container.set_allignment(input('Введите мировоззрение: '))
+    def fill_header(self, ch_name, cls, pl_name, bg, algmt):
+        self.header_container.set_char_name(ch_name)
+        self.header_container.add_class(cls)
+        self.header_container.set_player_name(pl_name)
+        self.header_container.set_background(bg)
+        self.header_container.set_allignment(algmt)
 
     def fill_stats(self):
         chars = []
-        print('Введите значения характеристик по порядку: СИЛА, ЛОВКОСТЬ, ТЕЛОСЛОЖЕНИЕ, ИНТЕЛЛЕКТ, МУДРОСТЬ, ХАРИЗМА')
         while len(chars) < 6:
             chars.extend([int(i) for i in ''.join([j for j in input() if (j.isdigit() or j == ' ')]).split()])
         for i in range(len(chars)):
             self.stats_container.stats.set_stat(i, chars[i])
+
+    def register(self):
+        self.fill_header()
+        self.fill_stats()
+
+
+class SheetsList:
+    def __init__(self):
+        self.__picked_sheets__ = dict()
+        self.__users_sheets__ = dict()
+        try:
+            self.load()
+        except Exception as e:
+            print(f'Could not dump sheets data: {e}')
+
+    def user_sheets(self, user: int):
+        try:
+            return self.__users_sheets__[user]
+        except Exception:
+            return None
+
+    def picked_sheet(self, user: int):
+        try:
+            return self.__picked_sheets__[user]
+        except Exception:
+            return None
+
+    def add_user_sheet(self, user: int, sheet: PlayerSheet):
+        if user in self.__users_sheets__.keys():
+            self.__users_sheets__[user].append(sheet)
+        else:
+            self.__users_sheets__[user] = [sheet]
+
+    def remove_user_sheet(self, user: int, sheet: PlayerSheet):
+        self.__users_sheets__[user].remove(sheet)
+
+    def pick(self, user: int, sheet: PlayerSheet):
+        self.__picked_sheets__[user] = sheet
+
+    def dump(self):
+        import json
+        with open('data/sheets.json', 'w') as json_file:
+            json.dump([self.__users_sheets__, self.__picked_sheets__], json_file, indent=4)
+
+    def load(self):
+        import json
+        with open('data/sheets.json', 'r') as json_file:
+            self.__users_sheets__, self.__picked_sheets__ = json.load(json_file)
+        #TODO: playersheet serialization
